5ae48eb6d3a6f18a0fbfdc9e104c417dd485ca65
aprizallahmad <aprizallahmad@gmail.com>
2024-01-31 10:49:45 +0700

    test async storage

--- screens/DetailKitab.jsx
+++ screens/DetailKitab.jsx
@@ -4,9 +4,12 @@ import { functionLog } from "../helpers/functionHelper";
 export const DetailKitab = ({ route }) => {
   let objData = route;
 
-  functionLog("data detail kitab", objData.params?.id);
+  functionLog("data detail kitab", objData?.params);
   return (
     <ScrollView>
+      <View>
+        <Text>{objData.params?.id}. {objData.params?.judul_indonesia}</Text>
+      </View>
       <View>
         <HTML source={{ html: objData.params?.isi_arab }} />
       </View>
