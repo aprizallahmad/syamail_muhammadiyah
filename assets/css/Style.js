9450ef23376638a5b1a4b5a9c1560a7a648d760c
aprizal ahmad <aprizallahmad@gmail.com>
2025-06-07 13:46:15 +0700

    fix Style.js

--- assets/css/Style.js
+++ assets/css/Style.js
@@ -2,7 +2,6 @@ import Color from "../color/Color";
 import { Dimensions } from "react-native";
 
 export const width = Dimensions.get("window").width;
-export const height = Dimensions.get("window").height;
 
 export const styles = {
   app: {
